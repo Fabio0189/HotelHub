a4889f13bf3e671aa5e68f44725630f3578c0297
faustovanella <faustovanella@yahoo.it>
2023-06-10 12:02:16 +0200

    bottone

--- app/src/main/java/com/esrcitazione/hotelhub/MainActivity.kt
+++ app/src/main/java/com/esrcitazione/hotelhub/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegistrazioneActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
