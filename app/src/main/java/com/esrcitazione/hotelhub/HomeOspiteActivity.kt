36af7dc00769ad5a33bc487a2f26c5b65a07984b
Fabio0189 <gaspare.v94@gmail.com>
2023-06-10 19:46:16 +0200

    update

---
+++ app/src/main/java/com/esrcitazione/hotelhub/HomeOspiteActivity.kt
@@ -0,0 +1,11 @@
+package com.esrcitazione.hotelhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeOspiteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_ospite)
+    }
+}
