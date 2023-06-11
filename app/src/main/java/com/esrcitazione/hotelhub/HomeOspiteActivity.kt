b3a834554ad7ffe6c0b0112c12dfe8d07d27711a
Fabio0189 <gaspare.v94@gmail.com>
2023-06-11 12:09:25 +0200

    fine giornata 1 parziale

--- app/src/main/java/com/esrcitazione/hotelhub/HomeOspiteActivity.kt
+++ app/src/main/java/com/esrcitazione/hotelhub/HomeOspiteActivity.kt
@@ -2,10 +2,21 @@ package com.esrcitazione.hotelhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import com.esrcitazione.hotelhub.databinding.ActivityHomeOspiteBinding
 
 class HomeOspiteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeOspiteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_ospite)
+        binding = ActivityHomeOspiteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.imageMenu.setOnClickListener {
+            binding.drawerLayout.openDrawer(Gravity.LEFT) // Or GravityCompat.START for support libraries
+        }
     }
 }
