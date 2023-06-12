da77a1cfc1bf60440def2a9f8afe7bd4575d091c
faustovanella <faustovanella@yahoo.it>
2023-06-12 22:27:31 +0200

    ridimensionamento immagini2

--- app/src/main/java/com/esrcitazione/hotelhub/PrenotaFragment.kt
+++ app/src/main/java/com/esrcitazione/hotelhub/PrenotaFragment.kt
@@ -1,25 +1,29 @@
 package com.esrcitazione.hotelhub
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.esrcitazione.hotelhub.databinding.FragmentPrenotaBinding
 import com.esrcitazione.hotelhub.databinding.ItemCameraBinding
 import androidx.recyclerview.widget.RecyclerView
 
-data class Camera(val tipo: String, val immagini: List<Int>)
+data class Camera(val tipo: String, val immagini: List<Int>, val prezzo: Double)
 
 class PrenotaFragment : Fragment() {
     private lateinit var binding: FragmentPrenotaBinding
+    private lateinit var cameraSelected: String
+    private var fatturazionePremuta = false
 
     private val camere = listOf(
-        Camera("Camera Singola", listOf(R.drawable.camerasingola1, R.drawable.camerasingola2, R.drawable.camerasingola3, R.drawable.camerasingola4)),
-        Camera("Camera Doppia", listOf(R.drawable.cameradoppia1, R.drawable.cameradoppia2, R.drawable.cameradoppia3, R.drawable.cameradoppia4)),
-        Camera("Camera Familiare", listOf(R.drawable.camerafamiliare1, R.drawable.camerafamiliare2, R.drawable.camerafamiliare3, R.drawable.camerafamiliare4))
+        Camera("Camera Singola", listOf(R.drawable.camerasingola1, R.drawable.camerasingola2, R.drawable.camerasingola3, R.drawable.camerasingola4), 50.0),
+        Camera("Camera Doppia", listOf(R.drawable.cameradoppia1, R.drawable.cameradoppia2, R.drawable.cameradoppia3, R.drawable.cameradoppia4), 100.0),
+        Camera("Camera Familiare", listOf(R.drawable.camerafamiliare1, R.drawable.camerafamiliare2, R.drawable.camerafamiliare3, R.drawable.camerafamiliare4), 200.0)
     )
 
     override fun onCreateView(
@@ -34,18 +38,38 @@ class PrenotaFragment : Fragment() {
                 layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                 adapter = CameraAdapter(camera)
             }
-            binding.layoutRecyclerViews.addView(recyclerView) //Assumendo che layoutRecyclerViews sia un LinearLayout nel tuo layout
+            binding.layoutRecyclerViews.addView(recyclerView)
         }
 
-        // Set up click handlers for your buttons
-        binding.buttonScegliCamera.setOnClickListener {
-            // Show the layoutRecyclerViews and the Spinner here
-            binding.layoutRecyclerViews.visibility = View.VISIBLE
-            binding.spinnerTipoCamera.visibility = View.VISIBLE
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.camera_types,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerTipoCamera.adapter = adapter
+        binding.spinnerTipoCamera.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                binding.buttonFatturazione.visibility = View.GONE
+                binding.textViewPrezzo.visibility = View.GONE
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                cameraSelected = adapter.getItem(position).toString()
+                binding.buttonFatturazione.visibility = if (!fatturazionePremuta) View.VISIBLE else View.GONE
+
+                val prezzoCamera = camere[position].prezzo
+                binding.textViewPrezzo.text = "Prezzo: $prezzoCamera €"
+                binding.textViewPrezzo.visibility = View.VISIBLE
+
+                if (fatturazionePremuta) {
+                    binding.buttonFatturazione.isEnabled = false
+                    binding.buttonFatturazione.alpha = 0.5f
+                }
+            }
         }
 
         binding.buttonFatturazione.setOnClickListener {
-            // Show the layout for the billing information here
             binding.textViewNome.visibility = View.VISIBLE
             binding.editTextNome.visibility = View.VISIBLE
             binding.textViewCognome.visibility = View.VISIBLE
@@ -55,12 +79,17 @@ class PrenotaFragment : Fragment() {
             binding.textViewCvv.visibility = View.VISIBLE
             binding.editTextCvv.visibility = View.VISIBLE
             binding.buttonConferma.visibility = View.VISIBLE
+            binding.buttonFatturazione.visibility = View.GONE
+            binding.buttonFatturazione.isEnabled = false
+            fatturazionePremuta = true
         }
 
         binding.buttonConferma.setOnClickListener {
+            val nome = binding.editTextNome.text.toString()
+            val cognome = binding.editTextCognome.text.toString()
             val numeroCarta = binding.editTextNumeroCarta.text.toString()
             val cvv = binding.editTextCvv.text.toString()
-            if (numeroCarta.length == 16 && cvv.length == 3) {
+            if (nome.isNotBlank() && cognome.isNotBlank() && numeroCarta.length == 16 && cvv.length == 3) {
                 Toast.makeText(context, "Pagamento effettuato", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(context, "Numero carta o CVV non validi", Toast.LENGTH_SHORT).show()
@@ -71,23 +100,18 @@ class PrenotaFragment : Fragment() {
     }
 
     inner class CameraAdapter(private val camera: Camera) : RecyclerView.Adapter<CameraAdapter.CameraViewHolder>() {
-
-        inner class CameraViewHolder(private val binding: ItemCameraBinding) : RecyclerView.ViewHolder(binding.root) {
-            fun bind(image: Int) {
-                binding.cameraImage.setImageResource(image)
-                binding.cameraText.text = camera.tipo
-            }
-        }
+        inner class CameraViewHolder(val binding: ItemCameraBinding) : RecyclerView.ViewHolder(binding.root)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CameraViewHolder {
             val binding = ItemCameraBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return CameraViewHolder(binding)
         }
 
-        override fun getItemCount(): Int = camera.immagini.size
+        override fun getItemCount() = camera.immagini.size
 
         override fun onBindViewHolder(holder: CameraViewHolder, position: Int) {
-            holder.bind(camera.immagini[position])
+            holder.binding.cameraImage.setImageResource(camera.immagini[position])
+            holder.binding.cameraText.text = "${camera.tipo} ${position + 1}"
         }
     }
 }
